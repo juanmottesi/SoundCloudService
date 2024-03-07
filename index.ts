3601b1e505a8825ea58f7bf5c8801c09a8438b98
Juan Pablo Mottesi <jmottesi@portfoliopersonal.com>
2024-03-07 11:50:08 -0300

    fix: exports

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { UserDraft, ArtistDraft, PlaylistDraft, SongDraft } from "./src/Draft";
+import SoundCloudService from "./src/SoundCloudService";
+
+export default {
+  SoundCloudService,
+  UserDraft,
+  ArtistDraft,
+  PlaylistDraft,
+  SongDraft,
+};
